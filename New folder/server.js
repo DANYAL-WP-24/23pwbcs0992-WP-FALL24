e5f2130fe36179a7ebf40f9c7cf51df5dbf5589f
DANYAL-WP-24 <danyalkhanself@gmail.com>
2025-01-10 22:17:32 -0700

    changed

---
+++ New folder/server.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/auth');
+const Connectdb=require('./config/db');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+//connect to db 
+Connectdb();
+
+app.get('/',(req,res)=>{
+    res.send("Hello")
+})
+// Routes
+app.use('/api', authRoutes);
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    
